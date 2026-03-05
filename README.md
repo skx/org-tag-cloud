f358d07b119c9a0cdb251d363e7ac12ac0f0b0d1
Steve Kemp <steve@steve.fi>
2026-03-05 19:44:05 +0200

    Updated repository with the latest version of the code from my private .dotfiles-repo

--- README.md
+++ README.md
@@ -1,28 +1,70 @@
 # org-tag-cloud
 
-This package allows you to easily generate, or update, a table of all the tags you've used within a particular `org-mode` file.
+This package is designed to allow you to maintain a "tag cloud"
+of all the tags stored within a particular `org-mode` file.
 
-
-## Installation / Usage
-
-* Save the file `org-tag-cloud.el` to a directory upon your load-path.
-* Load an org-file, and position the point at a suitable location.
-* Run `M-x org-tag-cloud-insert` to insert a source-block at the point.
-
-In the future you can update your tag-cloud by running `M-x org-tag-cloud-update`.  When you save your document in the future any tag-cloud will be automatically updated.
+The cloud is rendered as an `org-mode' table and can be updated
+automatically when the file is saved.  The rendered table will
+make each tag clickable via a newly-installed protocol handler
+for links of the form [[tag::foo].
 
 
 
-## Tag Search Results
+## Example Output
 
-The table will have a clickable entry, which will show you entries matching the given tag.  By default this will prompt you to permit the execution.
+This repository contains `example.org` showing the rendered result,
+but note that you will need to click the "RAW" link to view it in
+a readable format:
 
-You can avoid this via:
+* https://raw.githubusercontent.com/skx/org-tag-cloud/refs/heads/master/example.org
 
-```lisp
-(defun skx-org-mode-hook-eval-ok ()
-    (make-variable-buffer-local 'org-confirm-elisp-link-function)
-    (setq org-confirm-elisp-link-function nil))
 
-(add-hook 'org-mode-hook 'skx-org-mode-hook-eval-ok)
-```
+
+## Package Overview
+
+The cloud is rendered as an `org-mode` table and can be updated
+automatically when the file is saved.  The rendered table will
+make each tag clickable via a newly-installed protocol handler
+for links of the form [[tag::foo].
+
+There are three main functions of interest:
+
+* `org-tag-cloud-insert` - Insert a tag cloud at the current point.
+* `org-tag-cloud-update` - Update an existing cloud.
+* `org-tag-cloud-save-hook` - Something to add to save-hook to automate updates when files are saved.
+
+
+
+## Usage
+
+
+Require the package and add the save hook for org-mode files.
+
+If you're doing this the old-school way, having saved a copy of
+the package file to a directory upon your `load-path` you can configure
+it like so:
+
+    (require 'org-tag-cloud)
+    (add-hook 'before-save-hook #'org-tag-cloud-save-hook)
+
+
+If you're using `use-package` you would instead prefer something
+like this which has the same result:
+
+    (use-package org-tag-cloud
+     :after org
+     :straight t
+     :hook
+       (before-save-hook . org-tag-cloud-save-hook))
+
+
+Once loaded you'll find you can browse tag-search results via a new
+protocol `[[tag:foo]]` - this will open a buffer showing all the entries
+you have tagged with the value `foo`.
+
+
+
+## Limitations
+
+* There can only be one tag cloud in a specific document, because the name is fixed.
+* The tag cloud refers only to the current file, you cannot make a cloud of tags used across multiple `org-mode' files.
--- org-tag-cloud.el
+++ org-tag-cloud.el
@@ -1,61 +1,154 @@
-;; org-tag-cloud.el - Maintain a tag-cloud inside an org-mode file
+;;; org-people.el --- Easily maintain a tag-cloud of org-mode tags. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 - 2026  Steve Kemp
+
+;; Author: Steve Kemp
+;; Maintainer: Steve Kemp
+;; Version: 0.2
+;; Package-Requires: ((emacs "29.1") (org "9.0"))
+;; Keywords: outlines, contacts, people
+;; URL: https://github.com/skx/org-people
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package is designed to allow you to maintain a "tag cloud"
+;; of all the tags stored within a particular file.
 ;;
+;; The cloud is rendered as an `org-mode' table and can be updated
+;; automatically when the file is saved.  The rendered table will
+;; make each tag clickable via a newly-installed protocol handler
+;; for links of the form [[tag::foo].
 ;;
+;; There are three main functions of interest:
+;;
+;; `org-tag-cloud-insert' - Insert a tag cloud at the current point.
+;;
+;; `org-tag-cloud-update' - Update an existing cloud.
+;;
+;; `org-tag-cloud-save-hook' - Something to add to save-hook to
+;; automate this package.
+
+;;; Usage:
+
+;; Require the package and add the save hook for org-mode files:
+;;
+;; (require 'org-tag-cloud)
+;; (add-hook 'before-save-hook #'org-tag-cloud-save-hook)
+;;
+;; If you're using `use-package' you would instead prefer something
+;; like this which has the same result:
+;;
+;; (use-package org-tag-cloud
+;;  :after org
+;;  :straight t
+;;  :hook
+;;    (before-save-hook . org-tag-cloud-save-hook))
+
+;;; Limitations:
+
+;; There can only be one tag cloud in a specific document, because
+;; the name is fixed.
+;;
+;; The tag cloud refers only to the current file, you cannot make
+;; a cloud of tags used across multiple `org-mode' files.
+;;
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'org)
+
 
 (defun org-tag-cloud-insert ()
-  "Insert a block which will be used to generate a tag-contents at point.
+  "Insert a new block which will become a tag-cloud.
 
-The block can be executed via org-tag-cloud-update."
+The block will be inserted at the point, and will be immediately
+populated via a call to `org-tag-cloud-update'."
   (interactive "*")
-  (insert "
-#+NAME: org-tag-cloud
-#+BEGIN_SRC emacs-lisp :colnames '(Frequency Tag) :exports results
-(org-tag-cloud-populate)
-#+END_SRC
+  (insert "#+BEGIN: tagcloud
+#+END:
 "))
 
 (defun org-tag-cloud-update ()
-  "Update the existing tag-cloud, by executing the block named 'org-tag-cloud'.
+  "Update any existing tag-cloud.
 
-This will also be done automatically when the document is saved."
+This relies upon the fact that the tag-cloud has a static
+name, and can be found by that name with `org-find-dblock'."
   (interactive "*")
-  (save-excursion
-    (org-save-outline-visibility t
-      (if (member "org-tag-cloud" (org-babel-src-block-names))
-          (progn
-            (setq-local org-confirm-babel-evaluate nil)
-            (org-babel-goto-named-src-block "org-tag-cloud")
-            (org-babel-execute-src-block))
-        (message "There is no block named 'org-tag-cloud' present")))))
+  (if (org-find-dblock "tagcloud")
+      (org-update-dblock)))
 
-(defun org-tag-cloud-populate ()
-  "Return a suitable structure which can be used to generate a tag-cloud."
-  (let (tags count)
+
+;;; org-mode "tag:" link
+
+(defun org-tag--open-tag (tag)
+  "Handler invoked when a TAG: link is clicked."
+  (org-tags-view nil tag))
+
+
+;; Create a new "tag:" link
+(org-link-set-parameters
+ "tag"
+ :follow #'org-tag--open-tag)
+
+
+
+;;; dblock glue
+
+(defun org-dblock-write:tagcloud (_params)
+  "Called when a block of name tagcloud is to be processed.
+
+This is the magic that updates the tag-cloud."
+  (let ((tags (org-tag-cloud--collect)))
+    (insert "| Frequency | Tag |\n|-\n")
+    (dolist (row tags)
+      (insert (format "| %d | %s |\n"
+                      (car row)
+                      (cadr row))))
+                      (org-table-align)))
+
+
+(defun org-tag-cloud--collect ()
+  "Find the distinct tags, and their counts, within the current file."
+  (let (tags)
     (save-excursion
       (goto-char (point-min))
       (while (re-search-forward org-complex-heading-regexp nil t)
         (dolist (tag (org-get-tags))
-          (unless (string-collate-equalp tag "")
-            (push tag tags))))
-      (cl-loop with result
-               for tag in tags
-               do (push (list (cl-count tag tags
-                                        :test #'string=)
-                              (format "[[elisp:(org-tags-view nil \"%s\")][%s]]" tag tag))
-                        count)
-               collect
-               (setq result (cl-remove-duplicates count :test #'equal))
-               finally return
-               (cl-sort result #'> :key #'car)))))
+          (unless (string-empty-p tag)
+            (push tag tags)))))
+    (cl-loop for tag in (cl-remove-duplicates tags :test #'string=)
+             collect
+             (list (cl-count tag tags :test #'string=)
+                   (format "[[tag:%s][%s]]" tag tag))
+             into result
+             finally return (cl-sort result #'> :key #'car))))
 
 
-; Update tag-cloud on-save
-(defun org-tag-cloud-update-hook ()
-  (when (or (eq major-mode 'org-mode)
-            (eq major-mode 'org-diary-mode))
+;;; Utility
+(defun org-tag-cloud-save-hook ()
+  "If the current mode is derived from `org-mode' update the tag cloud.
+
+This will silently and safely do nothing if there is no `dblock' with the
+name 'tagcloud' within the document."
+  (if (derived-mode-p 'org-mode)
     (org-tag-cloud-update)))
 
-(add-hook 'before-save-hook #'org-tag-cloud-update-hook)
 
-; All done
 (provide 'org-tag-cloud)
+;;; org-tag-cloud.el ends here
